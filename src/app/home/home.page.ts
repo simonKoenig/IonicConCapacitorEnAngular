00827378f270932223e728ad37923d73ec4b9ea5
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-05-21 11:56:19 -0300

    Agrega persistencia de los datos con preferences de capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-home',
@@ -14,13 +15,46 @@ export class HomePage {
     private http: HttpClient,
   ) {}
 
-  // guardo el contenido de la api en la variable characters
   ngOnInit() {
-    this.http.get<any>('https://rickandmortyapi.com/api/character/?page=1')
-    .subscribe(res => {
-      console.log(res);
-      this.characters = res.results;
-    })
-  }
+    const fetchData = async () => {
+    const { value: datosExistentes } = await Preferences.get({ key: 'data' });
+    if (datosExistentes === null) {
+      this.http.get<any>('https://rickandmortyapi.com/api/character/?page=1')
+      .subscribe(res => {
+        console.log("api");
+        this.characters = res.results;
+        // Guardar los datos en las preferencias
+        const saveData = async () => {
+          await Preferences.set({
+            key: 'data',
+            value: JSON.stringify(this.characters),
+          });
+        };
+        saveData(); // Llama a la función para guardar los datos
+        //setData(res.results);
+        //setResults(res.results);
+      })
+      /*.catch(error => {
+        console.log('Error:', error);
+      })
+      .finally(() => {
+        setLoad(true);
+      });*/
+  
+    } else {
+      // Obtener los datos de las preferencias
+      //const parsedData = datosExistentes !== null ? JSON.parse(datosExistentes) : null;
 
+      // Mostrar los datos de las preferencias en la vista
+      this.characters = datosExistentes !== null ? JSON.parse(datosExistentes) : null;
+
+
+
+      //setData(parsedData);
+      //setResults(parsedData)
+      //setLoad(true);
+    }
+  };
+  fetchData(); // Llama a la función fetchData para iniciar el proceso
+  }
 }
