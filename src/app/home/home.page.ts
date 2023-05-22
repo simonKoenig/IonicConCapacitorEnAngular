60541e8b546702a5723a944172c96372469d99af
Enzo Defranco <enzolobod22@gmail.com>
2023-05-22 09:19:29 -0300

    Agrego Toast al eliminar personaje y al error de consulta en la API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Preferences } from '@capacitor/preferences';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-home',
@@ -14,43 +16,43 @@ export class HomePage {
 
   constructor(
     private http: HttpClient,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private toastController: ToastController
   ) {}
 
 
   ngOnInit() {
     const fetchData = async () => {
-    const { value: datosLocal } = await Preferences.get({ key: 'data' });
-    if (datosLocal === null) {
-      this.http.get<any>('https://rickandmortyapi.com/api/character/?page=1')
-      .subscribe(res => {
-        console.log("api");
-        this.characters = res.results;
-        // Guardar los datos en las preferencias
-        const saveData = async () => {
-          await Preferences.set({
-            key: 'data',
-            value: JSON.stringify(this.characters),
+      const { value: datosLocal } = await Preferences.get({ key: 'data' });
+      if (datosLocal === null) {
+        this.http.get<any>('https://rickandmortyapi.com/apiii/character/?page=1')
+          .subscribe({
+            next: (res) => {
+              console.log("api");
+              this.characters = res.results;
+              // Guardar los datos en las preferencias
+              const saveData = async () => {
+                await Preferences.set({
+                  key: 'data',
+                  value: JSON.stringify(this.characters),
+                });
+              };
+              saveData(); // Llama a la función para guardar los datos
+            },
+            error: (error) => {
+              console.error('Error en la consulta a la API:', error);
+              this.Toast('Error en la consulta a la API', 'error');
+            }
           });
-        };
-        saveData(); // Llama a la función para guardar los datos
-      })
-      /*.catch(error => {
-        console.log('Error:', error);
-      })
-      .finally(() => {
-        setLoad(true);
-      });*/
-  
-    } else {
-      // Mostrar los datos de las preferencias en la vista
-      console.log("guardado");
-      this.characters = datosLocal !== null ? JSON.parse(datosLocal) : null;
-      //setLoad(true);
-    }
-  };
-  fetchData(); // Llama a la función fetchData para iniciar el proceso
+      } else {
+        // Mostrar los datos de las preferencias en la vista
+        console.log("guardado");
+        this.characters = datosLocal !== null ? JSON.parse(datosLocal) : null;
+      }
+    };
+    fetchData(); // Llama a la función fetchData para iniciar el proceso
   }
+  
 
   async confirmarBorrado(id: number, nombre:string) {
     const alert = await this.alertController.create({
@@ -69,6 +71,7 @@ export class HomePage {
           text: 'Eliminar',
           handler: () => {
             this.borrar(id);
+            this.Toast(`Se ha eliminado ${nombre} exitosamente`, 'success');
           }
         }
       ]
@@ -91,6 +94,16 @@ export class HomePage {
     }
     console.log(this.characters);
     Preferences.set({ key: 'data', value: JSON.stringify(this.characters) });
+  }
+
+  async Toast(mensaje: string, tipo: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000, // Duración del Toast en milisegundos
+      position: 'bottom', // Posición del Toast en la pantalla (bottom, middle, top)
+      color: tipo === 'error' ? 'danger' : 'success'
+    });
+    toast.present();
   }
 
 }
